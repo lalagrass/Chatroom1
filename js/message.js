fd52336c40d728e271f1f564d64e739001a7c7cd
Yian Chen <yian_chen@trendmicro.com>
2022-05-20 22:51:03 +0800

    update layout

--- js/message.js
+++ js/message.js
@@ -1,17 +1,20 @@
-window.addEventListener('load', function () {
-    $("#submitmsg").on('submit', function(event){
-        event.preventDefault();
-        postMessage();
-     });
-});
-
-/*global WildRydes _config*/
-function postMessage() {
-    console.log('postMessage');
+function submitMessage() {
     let userfrom = document.getElementById('userfrom').value;
     let userto = document.getElementById('userto').value;
     let usermsg = document.getElementById('usermsg').value;
     let timestamp = new Date().toTimeString();
+
+    if (!usermsg) return;
+
+    postMessage(userfrom, userto, usermsg, timestamp);
+
+    appendMessage(userfrom, "right", usermsg, timestamp);
+    document.getElementById('usermsg').value = "";
+}
+
+/*global WildRydes _config*/
+function postMessage(userfrom, userto, usermsg, timestamp) {
+    console.log('postMessage');
     console.log('userfrom: ' + userfrom);
     console.log('userto: ' + userto);
     console.log('usermsg: ' + usermsg);
@@ -52,4 +55,23 @@ function completeRequest(result) {
 function displayUpdate(text) {
     console.log('displayUpdate');
     $('#updates').append($('<li>' + text + '</li>'));
+}
+
+function appendMessage(name, side, text, time) {
+    //   Simple solution for small apps
+    const msgHTML = `
+      <div class="msg ${side}-msg">  
+        <div class="msg-bubble">
+          <div class="msg-info">
+            <div class="msg-info-name">${name}</div>
+            <div class="msg-info-time">${time}</div>
+          </div>
+  
+          <div class="msg-text">${text}</div>
+        </div>
+      </div>
+    `;
+    let msgerChat = $("#msger-chat");
+    msgerChat[0].insertAdjacentHTML("beforeend", msgHTML);
+    msgerChat[0].scrollTop += 500;
 }
